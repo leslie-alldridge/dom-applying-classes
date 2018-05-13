a11ac646a654bc4d15eb0caadef7b7c7ce675822
Leslie Alldridge <Leslie@Leslie-Alldridges-MacBook-Air.local>
2018-05-13 15:40:07 +1200

    added fourth function to display box

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
    one()
    two()
    three ()
+   makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -31,3 +32,7 @@ function three () {
 }
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+var four = document.getElementsByClassName("invisible")[0];
+four.classList.add('visible')
+}
